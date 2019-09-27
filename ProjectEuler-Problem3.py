f359d8d1d2308b62195bfa2822b19018fdb8d3fc
Andrew Pang <andrew.y.pang@gmail.com>
2019-09-26 17:58:20 -0700

    p3 done

--- ProjectEuler-Problem3.py
+++ ProjectEuler-Problem3.py
@@ -1,8 +1,18 @@
-#The prime factors of 13195 are 5, 7, 13 and 29.
+# #The prime factors of 13195 are 5, 7, 13 and 29.
 
-#What is the largest prime factor of the number 600851475143 ?
+# #What is the largest prime factor of the number 600851475143 ?
 
-number = 600851475143
+# https://en.wikipedia.org/wiki/Primality_test
 
-while number !=:
-	pass
+# Let N be a positive integer. If N is composite then N has a prime factor between 2 and floor(sqrt(N)).
+# Thus, if N > 1 and N has no prime factors between 2 and floor(sqrt(N)) then N must be prime.
+
+n = 600851475143
+i = 2
+
+while i * i < n:
+    while n % i == 0: # if i is an even divisor of n then divide it
+        n = n / i
+    i = i + 1 # i++
+
+print(n)
